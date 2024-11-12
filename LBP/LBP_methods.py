dd3bdc761946c62cdb3a18abf735ec3d2140d900
villi02 <villiarnar1@gmail.com>
2024-11-12 19:15:22 +0100

    saving before checkout

--- LBP/LBP_methods.py
+++ LBP/LBP_methods.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage import feature
 
 
 def lbp(img, R=1):
@@ -12,7 +13,7 @@ def lbp(img, R=1):
     h, w = img.shape
 
     # Initialize LBP image
-    lbp_img = np.zeros((h, w), dtype=np.uint8)
+    lbp_img = np.zeros((h, w), dtype=np.float32)
 
     # Compute LBP for each pixel in the image
     for i in range(R, h - R):
@@ -41,7 +42,7 @@ def lbp(img, R=1):
     return lbp_img
 
 
-def lbp_hist(lbp_img):
+def hist(lbp_img):
     """
     Compute the LBP histogram for an LBP image
     :param lbp_img: LBP image
@@ -51,3 +52,20 @@ def lbp_hist(lbp_img):
     hist, _ = np.histogram(lbp_img, bins=256, range=(0, 256))
 
     return hist
+
+
+def compute_lbp(img, radius=1, n_points=8):
+    """
+    Compute Local Binary Pattern (LBP) using skimage's optimized function.
+    :param img: input grayscale image
+    :param radius: radius of LBP
+    :param n_points: number of points considered for LBP
+    :return: LBP image
+    """
+    # Calculate the number of sampling points
+    n_points = n_points * radius
+
+    # Compute LBP using skimage
+    lbp_img = feature.local_binary_pattern(img, n_points, radius, method="default")
+
+    return lbp_img
